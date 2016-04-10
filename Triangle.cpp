c19538581179cc69e960a80aa4d7a3db0477a547
Mac Groot <mcgroot70@gmail.com>
2016-04-10 13:36:55 -0700

    Writing triangle intersect function

--- Triangle.cpp
+++ Triangle.cpp
@@ -43,5 +43,39 @@ Triangle::renderGL()
 bool
 Triangle::intersect(HitInfo& result, const Ray& r,float tMin, float tMax)
 {
-    return false;
+    //First get vertices of triangle
+    
+    //Get the triangle by its index within the mesh it is a part of
+    TriangleMesh::TupleI3 ti3 = m_mesh->vIndices()[m_index];
+    
+    const Vector3& a = m_mesh->vertices()[ti3.x]; //vertex a of triangle
+    const Vector3& b = m_mesh->vertices()[ti3.y]; //vertex b of triangle
+    const Vector3& c = m_mesh->vertices()[ti3.z]; //vertex c of triangle
+    
+    //Compute how far away ray's origin is from the triangle's plane
+    //(or even if it hits the plane at all. Won't hit if ray parallel).
+    //First need to get normal of triangle
+    const Vector3& normal = Vector3::cross(b - a, c - a);
+    
+    //fill in shading normal in HitInfo result
+    result.N = normal;
+    
+    //Solve n dot (p - x) = 0, for p, where n = normal and x = ray
+    //First check if n dot d is zero. If it is, ray is parallel to triangle
+    float denom = dot(normal, r.d);
+    
+    if (denom == 0.0f)
+        return false;
+    
+    result.t = Vector3::dot(normal, a - r.o) / denom;
+    
+    //Assign hit location to HitInfo result
+    result.P = r.o + result.t * r.d;
+    
+    //Compute barycentric coordinates of result.P relative to this triangle.
+    //If alpha and beta both between 0 and 1, and alpha + beta <= 1, HIT
+    //Check notes 4/5/16 for setup of linear equation and how Cramer's rule is used
+    //to solve for each coordinate.
+    float alpha = 
+    
 }
