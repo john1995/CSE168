2ff23e3a9293e55ddd35a2a95d24d89e40e4cc93
John Leyson <jleyson@ucsd.edu>
2016-05-10 19:37:12 -0700

    Added bbox intersection

--- bbox.cpp
+++ bbox.cpp
@@ -1,4 +1,5 @@
 #include "bbox.h"
+#include <algorithm> 
 
 bbox::bbox()
 {
@@ -7,10 +8,44 @@ bbox::bbox()
 
 bbox::bbox(Vector3 max, Vector3 min)
 {
-    tmax = max;
-    tmin = min;
+    maxC = max;
+    minC = min;
 }
 
 bbox::~bbox()
 {
+}
+
+bool bbox::intersect(Ray r)
+{
+    Vector3 t1;
+    Vector3 t2;
+    Vector3 tmin;
+    Vector3 tmax;
+    
+    t1.x = (minC.x - r.o.x) / r.d.x;
+    t1.y = (minC.y - r.o.y) / r.d.y;
+    t1.z = (minC.z - r.o.z) / r.d.z;
+
+    t2.x = (maxC.x - r.o.x) / r.d.x;
+    t2.y = (maxC.y - r.o.y) / r.d.y;
+    t2.z = (maxC.z - r.o.z) / r.d.z;
+    
+    
+    tmax.x = std::max(t1.x, t2.x);
+    tmax.y = std::max(t1.y, t2.y);
+    tmax.z = std::max(t1.z, t2.z);
+    
+    tmin.x = std::min(t1.x, t2.x);
+    tmin.y = std::min(t1.y, t2.y);
+    tmin.z = std::min(t1.z, t2.z);
+    
+    float t_min = std::max(tmin.x, std::max(tmin.y, tmin.z));
+    float t_max = std::min(tmax.x, std::max(tmax.y, tmax.z));
+    
+    if (t_min < t_max)
+        return true;
+    else
+        return false;
+    
 }
--- bbox.h
+++ bbox.h
@@ -2,7 +2,9 @@
 #define CSE168_BBOX_H_INCLUDED
 
 #include "Vector3.h"
-
+#include "Ray.h"
+#include "Object.h"
+#include <vector>
 
 class bbox
 {
@@ -11,8 +13,12 @@ public:
     bbox();
     bbox(Vector3, Vector3);
     ~bbox();
-    Vector3   tmin;
-    Vector3   tmax;
+    Vector3   minC;
+    Vector3   maxC;
+    Object *obj;
+    bool intersect(Ray);
+    std::vector<bbox*> children;
+    bool isLeaf;
 
 };
 
